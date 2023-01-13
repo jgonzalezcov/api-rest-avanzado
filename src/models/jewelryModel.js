b8825dbb5595eb6a7977a763804495af92042da4
Juan Gonzalez Covarrubias <104181238+jgonzalezcov@users.noreply.github.com>
2023-01-13 18:57:29 -0300

    feat:models is created with your jewelryModel.js and model_db.sql files

---
+++ src/models/jewelryModel.js
@@ -0,0 +1,62 @@
+const { pool, format } = require('../helpers/connectDb')
+
+const getJewelry = async ({ limits = 10, order_by = 'id_ASC', page = 1 }) => {
+  const [campo, direccion] = order_by.split('_')
+  const offset = (page - 1) * limits
+  const formattedQuery = format(
+    'SELECT * FROM inventory order by %s %s LIMIT %s OFFSET %s',
+    campo,
+    direccion,
+    limits,
+    offset
+  )
+  try {
+    {
+      const result = await pool.query(formattedQuery)
+      return result
+    }
+  } catch (e) {
+    console.log(
+      'Error al obtener datos de tabla inventory: ',
+      e.code,
+      e.message
+    )
+    throw new Error(e)
+  }
+}
+
+const getFilterJewelry = async ({
+  id,
+  precio_min,
+  precio_max,
+  categoria,
+  metals,
+}) => {
+  let filtros = []
+  if (id) filtros.push(`id = ${id}`)
+  if (precio_max) filtros.push(`price <= ${precio_max}`)
+  if (precio_min) filtros.push(`price >= ${precio_min}`)
+  if (categoria) filtros.push(`category = '${categoria}'`)
+  if (metals) filtros.push(`metal = '${metals}'`)
+  let consulta = 'SELECT * FROM inventory'
+  if (filtros.length > 0) {
+    filtros = filtros.join(' AND ')
+    consulta += ` WHERE ${filtros}`
+  }
+  try {
+    const { rows: jewelrys } = await pool.query(consulta)
+    return jewelrys
+  } catch (e) {
+    console.log(
+      'Error al obtener datos filtrados de tabla inventory: ',
+      e.code,
+      e.message
+    )
+    throw new Error(e)
+  }
+}
+
+module.exports = {
+  getJewelry,
+  getFilterJewelry,
+}
---
+++ src/models/model_db.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE jewelry;
+\c jewelry
+CREATE TABLE inventory (id SERIAL, name VARCHAR(50), category
+VARCHAR(50), metal VARCHAR(50), price INT, stock INT);
+INSERT INTO inventory values
+(DEFAULT, 'Collar Heart', 'collar', 'oro', 20000 , 2),
+(DEFAULT, 'Collar History', 'collar', 'plata', 15000 , 5),
+(DEFAULT, 'Aros Berry', 'aros', 'oro', 12000 , 10),
+(DEFAULT, 'Aros Hook Blue', 'aros', 'oro', 25000 , 4),
+(DEFAULT, 'Anillo Wish', 'aros', 'plata', 30000 , 4),
+(DEFAULT, 'Anillo Cuarzo Greece', 'anillo', 'oro', 40000 , 2);
